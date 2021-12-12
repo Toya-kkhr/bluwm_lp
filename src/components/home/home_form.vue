b43c1f5e41468ee92443b8a0064be1d80ad1a5f6
Toya <toyack1128@gmail.com>
2021-12-12 16:42:13 +0900

    20211212

--- src/components/home/home_form.vue
+++ src/components/home/home_form.vue
@@ -6,17 +6,39 @@
             <v-card
             class="rounded-xl pa-6"
             >
-                <v-btn>LINE</v-btn>
-                <hr class="ma-2">
+                <v-btn
+                color="success"
+                height="50px"
+                width="50%"
+                >
+                LINEで相談
+                </v-btn>
+
+                <v-divider class="ma-5"></v-divider>
                 <v-form>
-                    <v-text-field>
+                    <v-card-subtitle>メールで相談</v-card-subtitle>
+                    <v-text-field
+                    label="お名前"
+                    >
                     </v-text-field>
-                    <v-text-field>
+
+                    <v-text-field
+                    label="メールアドレス"
+                    >
                     </v-text-field>
-                    <v-text-field>
-                    </v-text-field>
-                    <v-text-area>
-                    </v-text-area>
+
+                    <v-textarea
+                    label="お問合せ内容"
+                    >
+                    </v-textarea>
+
+                    <v-btn
+                    width="100%"
+                    height="50px"
+                    color="primary"
+                    >
+                        送信
+                    </v-btn>
                 </v-form>
             </v-card>
         </v-col>
