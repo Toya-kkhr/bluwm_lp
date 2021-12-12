9f350d741abcf9ccc7b671c0cac20a392679cec1
Toya <toyack1128@gmail.com>
2021-12-12 22:04:50 +0900

    20211212

--- src/components/home/home_hero.vue
+++ src/components/home/home_hero.vue
@@ -1,13 +1,16 @@
 <template>
-     <v-parallax src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg" >
+     <!-- <v-parallax src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg" > -->
+     <v-parallax :src= "require('@/assets/background3.jpg')">
  <v-row
     align="center"
     justify='center'
     >
     <v-col
+
     >
-                <h1>オンラインアイテムで結婚式を豊かに</h1>
+        <h1>Bluwm</h1>
+        <h3>「オンラインペーパーアイテム」<br>という選択をあなたに</h3>
     </v-col>
  </v-row>
- </v-parallax>
+     </v-parallax>
 </template>
