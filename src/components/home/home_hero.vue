5caa5e167eda3c4de118b175ad6bf6930bca6664
Toya <toyack1128@gmail.com>
2021-12-12 19:39:42 +0900

    20211212

--- src/components/home/home_hero.vue
+++ src/components/home/home_hero.vue
@@ -1,19 +1,13 @@
 <template>
-     <v-parallax src="@/assets/background.jpg">
+     <v-parallax src="@/assets/background2.jpg">
  <v-row
     align="center"
     justify='center'
     >
     <v-col
-    cols='6'
-
     >
         <div>
-            <v-card
-            class="rounded-xl"
-            >
                 <h1>オンラインアイテムで結婚式を豊かに</h1>
-            </v-card>
         </div>
     </v-col>
  </v-row>
