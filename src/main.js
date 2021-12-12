bcb49ceaffddd0a092efe11ef0d6e8461cea56ac
Toya <toyack1128@gmail.com>
2021-12-12 17:24:04 +0900

    20211212

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import VueGtag from 'vue-gtag'
 
 Vue.config.productionTip = false
+
+Vue.use(
+  VueGtag,
+  {
+    config: { id: 'G-971ZL6G3E9' }
+  },
+)
 
 new Vue({
   vuetify,
